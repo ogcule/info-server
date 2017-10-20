e671007e5cc06ef441112e5574e18fde29c43dea
ogcule <ogcule@outlook.com>
2017-10-20 18:55:59 +0100

    update

--- src/components/faq/Displayfaq.jsx
+++ src/components/faq/Displayfaq.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './faq.scss';
 
-const Displayfaq = () => (
-  <section className={styles.displayFaq}>
-    <div>
-      <header>Frequently asked questions</header>
-    </div>
-  </section>
+const DisplayFaq = props => (
+  <ul className={styles.displayFaq}>
+    {props.allFaq.map((obj) => {
+      const { id, question, answer } = obj;
+      return (
+        <ul key={id} data-id={id}>
+          <li className={styles.liHeading}>{question}</li>
+          <li className={styles.liAnswer}>{answer}</li>
+        </ul>
+    );
+  })}
+  </ul>
 );
 
-export default Displayfaq;
+DisplayFaq.propTypes = {
+  allFaq: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    question: PropTypes.string,
+    answer: PropTypes.string,
+  })),
+};
+DisplayFaq.defaultProps = {
+  allFaq: [{ id: 0, question: 'What happens if there is an error getting your information?', answer: 'You get this message' }],
+};
+export default DisplayFaq;
--- src/components/faq/Faq.jsx
+++ src/components/faq/Faq.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-// import styles from './faq.scss';
-// import Displayfaq from './Displayfaq';
-// import PropTypes from 'prop-types';
+import styles from './faq.scss';
+import DisplayFaq from './Displayfaq';
 import getAllFaqs from './../../util/api';
 
 
@@ -30,33 +29,19 @@ class Faq extends React.Component {
     if (!this.state.loaded) {
       return (
         <div>
-          <h2>Topics</h2>
+          <h2>Frequently asked questions</h2>
           <p>.....Loading</p>
         </div>
       );
     }
     console.log(this.state.faq);
     return (
-      <div>
-        <h2>Topics</h2>
-        {this.state.faq.map((obj) => {
-            const { id, question, answer } = obj;
-            return (
-              <ul key={id} data-id={id}>
-                <li>{question}</li>
-                <li>{answer}</li>
-              </ul>
-          );
-        })}
+      <div className={styles.flexContainer}>
+        <h2>Frequently asked questions</h2>
+        <DisplayFaq allFaq={this.state.faq} />
       </div>
     );
   }
 }
-// FaqList.propTypes = {
-//   question: PropTypes.arrayOf(PropTypes.string),
-// };
-// FaqList.defaultProps = {
-//   question: ['hello', 'world'],
-// };
 
 export default Faq;
